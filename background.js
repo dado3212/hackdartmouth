4a1edd5b9d4402bd6439670696d612075110ed28
Alex Beals <alexcbeals@gmail.com>
2016-09-25 15:02:49 -0400

    took out console stuff

--- background.js
+++ background.js
@@ -2,7 +2,6 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
    	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     	var url = tabs[0].url;
-    	console.log('Working on url: ' + url);
     	chrome.tabs.sendMessage(tabs[0].id, {action: "newWindow", url: url}, function(response) {
     	});
     });
