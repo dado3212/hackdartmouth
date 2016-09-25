a27b9b7f664dfb349d72f7fe43d3da914c153c03
Alex Beals <alexcbeals@gmail.com>
2016-09-24 22:48:04 -0400

    working with sample data

--- main.js
+++ main.js
@@ -6,12 +6,47 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     str = str.replace(/<(?:.|\n)*?>/gm, ' ');
     str = str.replace(/\n/gm, ' ')
     
-    message.innerText = str;
+    // message.innerText = str;
+
+    var colors = ['#AC1212', '#F37310', '#EBC621', '#9EC430', '#4E9A26'];
+    var icons = ['red', 'orange', 'yellow', 'yellowgreen', 'green'];
+
+    var data = {
+      'audience': 5,
+      'anger': 4.1,
+      'cyberbullying': 3,
+      'profanity': 2,
+      'overall': 3.4
+    };
+
+    var body = document.querySelector('body');
+    var mainScore = document.querySelector('#mainScore');
+
+    var anger = document.querySelector('#anger');
+    var audience = document.querySelector('#audience');
+    var cyberbullying = document.querySelector('#cyberbullying');
+    var profanity = document.querySelector('#profanity');
+
+    var color = colors[Math.round(data.overall) - 1];
+    var iconColor = icons[Math.round(data.overall) - 1];
+
+    body.style.backgroundColor = color;
+    mainScore.innerText = data.overall;
+    mainScore.style.color = color;
+
+    anger.querySelector('.score').innerText = data.anger;
+    anger.querySelector('.bar span').style.minWidth = (data.anger/5) * 3 + 'em';
+    audience.querySelector('.score').innerText = data.audience;
+    audience.querySelector('.bar span').style.minWidth = (data.audience/5) * 3 + 'em';
+    cyberbullying.querySelector('.score').innerText = data.cyberbullying;
+    cyberbullying.querySelector('.bar span').style.minWidth = (data.cyberbullying/5) * 3 + 'em';
+    profanity.querySelector('.score').innerText = data.profanity;
+    profanity.querySelector('.bar span').style.minWidth = (data.profanity/5) * 3 + 'em';
 
     chrome.browserAction.setIcon({
       path: {
-        '19': 'icons/green_19.png',
-        '38': 'icons/green_38.png'
+        '19': 'icons/' + iconColor + '_19.png',
+        '38': 'icons/' + iconColor + '_38.png'
       },
     });
 
