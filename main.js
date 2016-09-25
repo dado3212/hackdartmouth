d03393fb1d7592e2c320180c814b81e0e9e0050b
Alex Beals <alexcbeals@gmail.com>
2016-09-25 12:17:10 -0400

    added small things

--- main.js
+++ main.js
@@ -8,64 +8,70 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     
     // message.innerText = str;
 
-    var urlString = window.location.href;
-    $.post("/api/v0.1/rating", {url:urlString, text:str },
-    function(response,status){ 
-    alert("*----Received Data----*\n\nResponse : " + response+"\n\nStatus : " + status);
-    });
-    
-    // var colors = ['#AC1212', '#F37310', '#EBC621', '#9EC430', '#4E9A26'];
-    var colors = ['#4E9A26', '#9EC430', '#EBC621', '#F37310', '#AC1212'];
-    // var icons = ['red', 'orange', 'yellow', 'yellowgreen', 'green'];
-    var icons = ['green', 'yellowgreen', 'yellow', 'orange', 'red'];
+    xhr = new XMLHttpRequest();
+    // xhr.open("POST", "http://surfshield-env.us-east-1.elasticbeanstalk.com/api/v0.1/rating", true);
+    xhr.open("POST", "http://alexbeals.com/test.php", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () { 
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          var data = JSON.parse(xhr.responseText);
 
-    var data = {
-      'audience': 5,
-      'anger': 4.1,
-      'cyberbullying': 3,
-      'profanity': 2,
-      'overall': 1
-    };
+          // var colors = ['#AC1212', '#F37310', '#EBC621', '#9EC430', '#4E9A26'];
+          var colors = ['#4E9A26', '#9EC430', '#EBC621', '#F37310', '#AC1212'];
+          // var icons = ['red', 'orange', 'yellow', 'yellowgreen', 'green'];
+          var icons = ['green', 'yellowgreen', 'yellow', 'orange', 'red'];
 
-    var body = document.querySelector('body');
-    var mainScore = document.querySelector('#mainScore');
+          // var data = {
+          //   'audience': 5,
+          //   'anger': 4.1,
+          //   'cyberbullying': 3,
+          //   'profanity': 2,
+          //   'overall': 1
+          // };
 
-    var anger = document.querySelector('#anger');
-    var audience = document.querySelector('#audience');
-    var cyberbullying = document.querySelector('#cyberbullying');
-    var profanity = document.querySelector('#profanity');
+          var body = document.querySelector('body');
+          var mainScore = document.querySelector('#mainScore');
 
-    var color = colors[Math.round(data.overall) - 1];
-    var iconColor = icons[Math.round(data.overall) - 1];
+          var anger = document.querySelector('#anger');
+          var audience = document.querySelector('#audience');
+          var cyberbullying = document.querySelector('#cyberbullying');
+          var profanity = document.querySelector('#profanity');
 
-    body.style.backgroundColor = color;
-    mainScore.innerText = data.overall;
-    mainScore.style.color = color;
+          var color = colors[Math.round(data.overall) - 1];
+          var iconColor = icons[Math.round(data.overall) - 1];
 
-    anger.querySelector('.score').innerText = data.anger;
-    anger.querySelector('.bar span').style.minWidth = (data.anger/5) * 3 + 'em';
-    audience.querySelector('.score').innerText = data.audience;
-    audience.querySelector('.bar span').style.minWidth = (data.audience/5) * 3 + 'em';
-    cyberbullying.querySelector('.score').innerText = data.cyberbullying;
-    cyberbullying.querySelector('.bar span').style.minWidth = (data.cyberbullying/5) * 3 + 'em';
-    profanity.querySelector('.score').innerText = data.profanity;
-    profanity.querySelector('.bar span').style.minWidth = (data.profanity/5) * 3 + 'em';
+          body.style.backgroundColor = color;
+          mainScore.innerText = data.overall;
+          mainScore.style.color = color;
 
-    chrome.browserAction.setIcon({
-      path: {
-        '19': 'icons/' + iconColor + '_19.png',
-        '38': 'icons/' + iconColor + '_38.png'
-      },
-    });
+          anger.querySelector('.score').innerText = data.anger;
+          anger.querySelector('.bar span').style.minWidth = (data.anger/5) * 3 + 'em';
+          audience.querySelector('.score').innerText = data.audience;
+          audience.querySelector('.bar span').style.minWidth = (data.audience/5) * 3 + 'em';
+          cyberbullying.querySelector('.score').innerText = data.cyberbullying;
+          cyberbullying.querySelector('.bar span').style.minWidth = (data.cyberbullying/5) * 3 + 'em';
+          profanity.querySelector('.score').innerText = data.profanity;
+          profanity.querySelector('.bar span').style.minWidth = (data.profanity/5) * 3 + 'em';
 
-    chrome.notifications.create('reminder', {
-        type: 'basic',
-        iconUrl: 'icons/Main.png',
-        title: 'Here is the score',
-        message: 'Site is Safe'
-     }, function(notificationId) {
-      timer = setTimeout(function(){chrome.notifications.clear(notificationId);}, 2500);
-     });
+          chrome.browserAction.setIcon({
+            path: {
+              '19': 'icons/' + iconColor + '_19.png',
+              '38': 'icons/' + iconColor + '_38.png'
+            },
+          });
+
+          // chrome.notifications.create('reminder', {
+          //   type: 'basic',
+          //   iconUrl: 'icons/Main.png',
+          //   title: 'Here is the score',
+          //   message: 'Site is Safe'
+          // }, function(notificationId) {
+          //   timer = setTimeout(function(){chrome.notifications.clear(notificationId);}, 1000);
+          // });
+      }
+    }
+    var d = JSON.stringify({ url: 'test', text: str });
+    xhr.send(d);
   } else if (request.action == "google") {
     chrome.notifications.create('reminder', {
         type: 'basic',
